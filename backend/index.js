e56184816047717606eb59a60088f67c1d726f58
Nimsaramahagedara <nimsaramahagedara@gmail.com>
2023-08-25 16:48:39 +0530

    Supplier and Stock

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 import express from 'express'
 import dotenv from 'dotenv'
+import Stockrouter from "./Routes/StockRoutes.js";
+import morgan from "morgan";
+import SupplierRouter from "./Routes/SupplierRoutes.js";
+
 
 const app = express()
 app.use(express.json())
+app.use(morgan('dev'));
 
 dotenv.config()
 
@@ -12,3 +17,6 @@ mongoose.connect(process.env.DB, {
 })
 .then(()=>app.listen(process.env.PORT, ()=>console.log(`Listening at ${process.env.PORT}`)))
 .catch((err)=>console.log(err))
+
+app.use('/stock', Stockrouter);
+app.use('/supplier', SupplierRouter);
