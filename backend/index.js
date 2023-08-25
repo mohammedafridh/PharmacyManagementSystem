66d706cb86dfe85c98336bc78c129e9e6161423d
Afridh <afridh7788@gmail.com>
2023-08-25 16:39:03 +0530

    adding admin backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import express from 'express'
 import dotenv from 'dotenv'
+import Admin from './Routes/AdminRoute.js'
 
 const app = express()
 app.use(express.json())
@@ -12,3 +13,6 @@ mongoose.connect(process.env.DB, {
 })
 .then(()=>app.listen(process.env.PORT, ()=>console.log(`Listening at ${process.env.PORT}`)))
 .catch((err)=>console.log(err))
+
+
+app.use('/admin',Admin)
