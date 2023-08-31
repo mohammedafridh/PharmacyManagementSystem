8a99772743cf740f1c01d0ed6d7a9038738b2852
Afridh <afridh7788@gmail.com>
2023-08-31 14:34:32 +0530

    adding create purchase order

--- frontend/src/Pages/Purchase/CreatePurchaseOrder.jsx
+++ frontend/src/Pages/Purchase/CreatePurchaseOrder.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './CreatePurchaseOrder.css'
 import { Button, Select, Input, InputNumber } from 'antd';
+import CreatePurchaseOrderPDF from '../../components/PDF Generations/CreatePurchaseOrderPDF';
 
 const CreatePurchaseOrder = () => {
+
+  const[allItems,setAllItems] = useState([])
+  const[companyName,setCompanyName] = useState('')
+  const[item, setItem] = useState('')
+  const[boxes, setBoxes] = useState('')
+  const[units, setUnits] = useState('')
+
+  console.log(boxes)
+
+  const handleCompanyName = (value)=>{
+    setCompanyName(value)
+  }
+
+  const handleSelectChange = (value)=>{
+    setItem(value)
+  }
+
+  const handlePurchaseOrder = (e)=>{
+    e.preventDefault()
+
+    const newItem = {
+      itemName:item,
+      boxes:boxes,
+      units:units
+    }
+
+    setAllItems((prevItems)=>[...prevItems, newItem]);
+  }
+
+  useEffect(() => {
+    console.log(allItems); // Log the updated state in the effect
+  }, [allItems]);
+
+
   return (
     <div className="createPurchaseOrderContents">
-      <form className="insertForm">
+      <form className="insertForm" onSubmit = {handlePurchaseOrder}>
 
         <Select
           showSearch
@@ -18,10 +53,13 @@ const CreatePurchaseOrder = () => {
           filterSort={(optionA, optionB) =>
             (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
           }
+
+          onChange = {handleCompanyName}
+
           options={[
             {
-              value: '1',
-              label: 'Not Identified',
+              value: 'Envicta Pharmacy',
+              label: 'Envicta Pharmacy',
             },
             {
               value: '2',
@@ -43,6 +81,9 @@ const CreatePurchaseOrder = () => {
             filterSort={(optionA, optionB) =>
               (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
             }
+
+            onChange={handleSelectChange}
+
             options={[
               {
                 value: '1',
@@ -54,13 +95,19 @@ const CreatePurchaseOrder = () => {
               }
             ]}
           />
-          <InputNumber placeholder='No of Boxes' style={{ width: '50%' }} />
-          <InputNumber placeholder='No of Units (optional)' style={{ width: '50%' }} />
+          <InputNumber placeholder='No of Boxes' style={{ width: '50%' }} onChange={(value) => setBoxes(value)}/>
+          <InputNumber placeholder='No of Units (optional)' style={{ width: '50%' }} onChange={(value) => setUnits(value)}/>
 
           <Button type="primary" htmlType="submit">
             Add
           </Button>
         </div>
+        
+        {/* Display and PDF part */}
+        <CreatePurchaseOrderPDF 
+          name = {companyName}
+          all = {allItems}
+        />
 
       </form>
     </div>
